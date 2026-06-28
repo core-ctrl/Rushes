65a64f9f7355f6f1dd0444ffdd2d4873fbb496e0
core-ctrl <parupallisaiharshitha@gmail.com>
2026-06-29 01:31:12 +0530

    fix: restore ClapperLoader to pass build, fix routing freeze, and attach local video stream

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useRouter } from 'next/router';
 import { Provider, useDispatch, useSelector } from "react-redux";
 import { AnimatePresence, motion } from "framer-motion";
